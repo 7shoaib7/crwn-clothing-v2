970b8ef2b115e83abd9c3cc8258c54de7d4c8de7
7shoaib7 <69447663+7shoaib7@users.noreply.github.com>
2023-01-26 20:46:20 +0530

    redux-thunk-added

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import thunk from 'redux-thunk';
+
 
 import { rootReducer } from './root-reducer';
 
